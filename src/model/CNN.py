dc0201dc465bb2e4105022ed36acf4e218dfc77d
Harjot <hsingh172000@gmail.com>
2021-05-27 21:25:46 -0700

    Created and trained the model

---
+++ src/model/CNN.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+import sys
+import os
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Reshape
+from PIL import Image
+
+if (os.path.exists('CNN_model')):
+    # If the model exists load it in
+    model = keras.models.load_model('CNN_model')
+
+    # Get the path to image being classified
+    path = sys.argv[1]
+
+    # Load the img as grayscale and dump into NP array
+    img = Image.open(path).convert('L')
+    m = np.array(img)
+
+    # Make the prediction
+    label = np.argmax(model.predict(m.reshape(1,28,28,1)))
+    print(label)
+else:
+    # Otherwise train a model
+    print('Creating a new CNN model...')
+    # Load in the MNIST dataset
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+    
+    # Convert X to be of typle float32 since original uint8 was not supported by model
+    X = tf.cast(X_train, tf.float32)
+
+    # Because we have 10 outputs, we need each row in Y to be 10 columns long that OHE the represented digit
+    Y = tf.keras.utils.to_categorical(Y_train, num_classes=10)
+
+    # Create the CNN
+    model = keras.Sequential(
+        [
+        Reshape((28,28,1), input_shape=(28,28)),
+        Conv2D(filters=128, kernel_size=2, strides=2, activation='relu'),
+        MaxPooling2D(),
+        Conv2D(filters=64, kernel_size=2, strides=2, activation='relu'),
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dense(10, activation='softmax')
+        ]
+    )
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Fit the model
+    model.fit(X, Y, epochs=10, batch_size=32, validation_split=0.2)
+
+    # Save the model
+    model.save('CNN_model')
