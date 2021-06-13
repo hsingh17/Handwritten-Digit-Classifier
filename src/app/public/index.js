5715846ba04935e38740ff2d8fff3e5f57501a18
Harjot <hsingh172000@gmail.com>
2021-06-13 16:37:06 -0700

    Center canvas. Fix coordinate space for the canvas. Add clear button functionality. Need to keep working on the front-end and responsiveness in mobile.

--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,30 +1,50 @@
-const canvas = document.getElementById('canvas')
-const btn = document.getElementById('btn')
 
-const ctx = canvas.getContext('2d')
-ctx.canvas.width = window.innerWidth
-ctx.canvas.height = window.innerHeight
+// <----------------------- CONSTANTS ----------------------->
+const canvas = document.getElementById('canvas'),
+    clear_btn = document.getElementById('clear-btn'),
+    btn_container_height = document.getElementById('btns-container').offsetHeight,
+    ctx = canvas.getContext('2d'),
+    N = 4,
+    M = 2,
+    DPI = window.devicePixelRatio,
+    width = Math.max(384, window.innerWidth / N), 
+    height  = Math.max(344, window.innerHeight / M)
+    
+// <----------------------- VARIABLES ----------------------->
+let width_offset = (window.innerWidth / 2) - (width / 2),
+    height_offset = ((window.innerHeight / 2) - (btn_container_height / 2)) - (height / 2),
+    mouse_down = false,
+    prev_x = -1,
+    prev_y = -1
 
-let mouse_down = false
-let prev_x = -1
-let prev_y = -1
+ctx.canvas.width = width * DPI
+ctx.canvas.height = height * DPI    
+canvas.style.width = width + 'px'
+canvas.style.height = height + 'px'
+ctx.scale(DPI, DPI)
 
-canvas.addEventListener('mousedown', (e) => {
-    let [x, y] = [e.clientX, e.clientY]
+window.addEventListener('mousedown', e => {
+    let [x, y] = [e.clientX-width_offset, e.clientY-height_offset]
     mouse_down = true
     prev_x = x
     prev_y = y
 })
 
-canvas.addEventListener('mouseup', () => {
+window.addEventListener('mouseup', () => {
     mouse_down = false
 })
 
-canvas.addEventListener('mousemove', (e) => {
-    let [cur_x, cur_y] = [e.clientX, e.clientY]
+window.addEventListener('resize', () => {
+    width_offset = (window.innerWidth / 2) - (width / 2)
+    height_offset = ((window.innerHeight / 2) - (btn_container_height / 2)) - (height / 2)
+})
+
+canvas.addEventListener('mousemove', e => {
+    let [cur_x, cur_y] = [e.clientX-width_offset, e.clientY-height_offset]
+    console.log(cur_x, cur_y, prev_x, prev_y)
     if (mouse_down) {
         ctx.beginPath()
-        ctx.lineWidth = 3
+        ctx.lineWidth = 5
         ctx.moveTo(prev_x, prev_y)
         ctx.lineTo(cur_x, cur_y)
         ctx.stroke()   
@@ -32,4 +52,8 @@ canvas.addEventListener('mousemove', (e) => {
         prev_x = cur_x
         prev_y = cur_y
     }
+})
+
+clear_btn.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 })
